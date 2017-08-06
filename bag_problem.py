1daf22ed2885d0b533eed876f402129d40c35a3d
aw10279 <aw10279@163.com>
2017-08-06 17:00:37 +0800

    add bag-problem-complete condition,finished.

--- bag_problem.py
+++ bag_problem.py
@@ -17,6 +17,9 @@ def bagpro(W, w, v):
 
 #bagpro(20, [5,4,3,6,5,6,2], [7,5,5,3,5,1,9])
 
+
+
+
 #--------------------背包问题变形，将给定的一组数字平分成2组，实现各组和的差值最小-----------------------------------
 #--------------------要求2组平均，即选择固定数量的数字使其总和在不超过sum/2的前提下最大化，即转化为经典背包问题------
 def bagpro_trans(L):
@@ -36,4 +39,26 @@ def bagpro_trans(L):
     print('sum = ', sum)
     print('minimum difference = ', abs(sum-2*matrix[len(L)][len(L)//2][sum//2]))
 
-bagpro_trans([5,8,3,7,7,9,2])
+#bagpro_trans([5,8,3,7,7,9,2])
+
+
+
+#--------------------完全背包问题,一维数组即可完成记录---------------------------------------------------------------
+
+
+def bagpro_complete(W, w, v):
+    sheet = [0 for col in range(W+1)]  #建一维数组并初始化0
+    
+    for i in range(1, W+1):  #遍历所有容量可能
+        L = [0 for col in range(len(w))]
+        for j in range(len(w)):  #因为每种都有无限多个，所以每个容量都要考察所有物品，才能判断放哪个最合适
+            if w[j] <= i:     #容量如果装得下
+                L[j] = max(sheet[i-1], sheet[i-w[j]]+v[j])  #每种物品都要比较放和不放哪个更大，并记录
+            else:
+                L[j] = sheet[i-1]  #装不下就延续之前的最佳数值
+
+        sheet[i] = max(L)  #最后将最大值放入sheet，完成本次容量的判断
+
+    print(sheet)
+
+bagpro_complete(10, [3,2,3], [6,4,1])
